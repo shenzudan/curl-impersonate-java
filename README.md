59c5bbff32c091e6e1ff719de71c1be92062320f
stan <admin@stanwind.com>
2024-04-03 11:13:38 +0800

    1. init

---
+++ README.md
@@ -0,0 +1,4 @@
+# curl-impersonate-java
+
+### Java binding for curl-impersonate
+模仿浏览器tls/ja3/http2 指纹识别
---
+++ src/main/java/com/stanwind/Main.java
@@ -0,0 +1,14 @@
+package com.stanwind;
+
+/**
+ * ${NAME} ${description}
+ *
+ * @author : stan
+ * @version : 1.0
+ * @date :  2024/4/3 11:09
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
